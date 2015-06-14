ae7e67bec89a1b40160e6c660dad908956c4423a
Jared Hanson <jaredhanson@gmail.com>
2015-06-14 15:46:29 -0700

    Less bullshit.

--- lock.js
+++ lock.js
@@ -1,7 +1,6 @@
-var WebSocket = require('faye-websocket');
-var tessel = require('tessel')
-//var ws = new WebSocket('ws://172.16.94.196:8084');
-var ws = new WebSocket.Client('ws://172.16.94.196:8084/');
+var WebSocket = require('ws');
+//var tessel = require('tessel')
+var ws = new WebSocket('ws://172.16.94.196:8084');
 
 ws.on('open', function open() {
   console.log('OPEN!!!');
@@ -13,31 +12,3 @@ ws.on('error', function open(err) {
   console.log(err.message);
   //ws.send('something');
 });
-
-/*
-var io = require('ws');
-
-var socket = io('http://172.16.94.196:8084');
-
-socket.on('connect', function(){
-  console.log('CONNECTED!');
-});
-
-socket.on('disconnect', function(){
-  console.log('DISCONNECTED!');
-});
-
-socket.on('connect_error', function(err){
-  console.log('FAILED TO CONNECT');
-  console.log(err);
-});
-
-socket.on('news', function (data) {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
-});
-
-socket.on('lock', function (data) {
-  console.log('LOCK ON THE TESSEL');
-});
-*/
