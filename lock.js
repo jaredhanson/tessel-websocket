5b7c3b2b009ddedd491387e34e3655389b7b7638
Jared Hanson <jaredhanson@gmail.com>
2015-06-14 15:03:31 -0700

    Socket.io code.

--- lock.js
+++ lock.js
@@ -1,8 +1,25 @@
 var io = require('socket.io-client');
 
-var socket = io('http://127.0.0.1:8084');
+var socket = io('http://172.16.94.196:8084');
+
+socket.on('connect', function(){
+  console.log('CONNECTED!');
+});
+
+socket.on('disconnect', function(){
+  console.log('DISCONNECTED!');
+});
+
+socket.on('connect_error', function(err){
+  console.log('FAILED TO CONNECT');
+  console.log(err);
+});
 
 socket.on('news', function (data) {
   console.log(data);
   socket.emit('my other event', { my: 'data' });
+});
+
+socket.on('lock', function (data) {
+  console.log('LOCK ON THE TESSEL');
 });
